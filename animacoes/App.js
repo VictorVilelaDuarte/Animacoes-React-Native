b1db1c57f7eb550728531f42eb2d203267aa3f8b
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-11-09 15:44:29 -0300

    Animação timing

--- animacoes/App.js
+++ animacoes/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Animated } from 'react-native';
+
+// Animated-> Animated.View, Animated.Text, Animated.Image, Animated.ScrollView
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
+    padding: 30,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  ball: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: '#F00',
   },
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.welcome}>Abaixo</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    ballY: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    const { ballY } = this.state;
+    Animated.timing(ballY, {
+      toValue: 500,
+      duration: 1000,
+    }).start();
+  }
+
+  render() {
+    const { ballY } = this.state;
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.ball, { top: ballY }]} />
+      </View>
+    );
+  }
 }
